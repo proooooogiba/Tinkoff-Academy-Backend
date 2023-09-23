b9ff830a8d303c258eeba2126fb06a71f26b24bd
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-09-23 11:29:59 +0300

    Сomments have been corrected

--- src/main/scala/solution.scala
+++ src/main/scala/solution.scala
@@ -4,75 +4,65 @@ trait GreatHouse {
 }
 
 trait Wealth {
-  var moneyAmount: Double
-  var armyForces: Double
+  val moneyAmount: Double
+  val armyForces: Double
 }
 
 trait MakeWildFire {
   this: GreatHouse =>
-  def makeWildFire: Wealth = {
-    wealth.armyForces *= 1.5
-    wealth
-  }
+  def makeWildFire: Wealth = Wealth(wealth.moneyAmount, wealth.armyForces * 1.5)
 }
 
 trait BorrowMoney {
   this: GreatHouse =>
-  def borrowMoney: Wealth = {
-    wealth.moneyAmount *= 1.7
-    wealth
-  }
+  def borrowMoney: Wealth = Wealth(wealth.moneyAmount * 1.7, wealth.armyForces)
 }
 
 trait CallDragon {
   this: GreatHouse =>
-  def callDragon: Wealth = {
-    wealth.armyForces *= 2
-    wealth
-  }
+  def callDragon: Wealth = Wealth(wealth.moneyAmount, wealth.armyForces * 2)
 }
 
-final case class Targaryen(name: String, wealth: Wealth)
+final case class Targaryen(name: String = "Таргариены", wealth: Wealth)
     extends GreatHouse
     with MakeWildFire
-    with CallDragon {
-  def apply(name: String, targaryenMove: Wealth): Targaryen =
-    Targaryen(name, targaryenMove)
-}
+    with CallDragon
 
-final case class Lannisters(name: String, wealth: Wealth)
+final case class Lannisters(name: String = "Ланистеры", wealth: Wealth)
     extends GreatHouse
     with MakeWildFire
-    with BorrowMoney {
-  def apply(name: String, lannistersMove: Wealth): Lannisters =
-    Lannisters(name, lannistersMove)
-}
+    with BorrowMoney
 
-class GameOfThrones(targaryen: Targaryen, lannisters: Lannisters) {
-  private var moveNumber = 0
+class GameOfThrones(
+    val targaryen: Targaryen,
+    val lannisters: Lannisters,
+    private val moveNumber: Int = 0
+) {
+
   def nextTurn(targaryenMove: Wealth)(lannistersMove: Wealth): GameOfThrones = {
-    moveNumber += 1
     new GameOfThrones(
-      Targaryen(targaryen.name, targaryenMove),
-      Lannisters(lannisters.name, lannistersMove)
+      Targaryen(wealth = targaryenMove),
+      Lannisters(wealth = lannistersMove),
+      moveNumber + 1
     )
   }
 }
-object wealthLanissters extends Wealth {
-  override var moneyAmount: Double = 35.0
-  override var armyForces: Double = 10.0
-}
 
-object wealthTargaryen extends Wealth {
-  override var moneyAmount: Double = 10.0
-  override var armyForces: Double = 18.0
+case class WealthImpl(moneyAmount: Double, armyForces: Double) extends Wealth
+
+object Wealth {
+  def apply(moneyAmount: Double, armyForces: Double): Wealth =
+    WealthImpl(moneyAmount, armyForces)
 }
 
 object solution extends App {
-  private val tar = Targaryen("Таргариены", wealthTargaryen)
-  private val lan = Lannisters("Ланистеры", wealthLanissters)
-  private var gameOfThrones = new GameOfThrones(tar, lan)
-  gameOfThrones = gameOfThrones
-    .nextTurn(tar.callDragon)(lan.makeWildFire)
-    .nextTurn(tar.callDragon)(lan.borrowMoney)
+
+  private val got = new GameOfThrones(
+    Targaryen(wealth = Wealth(35.0, 10.0)),
+    Lannisters(wealth = Wealth(18.0, 10.0))
+  )
+
+  val got2 = got.nextTurn(got.targaryen.callDragon)(got.lannisters.borrowMoney)
+  val got3 = got2.nextTurn(got2.targaryen.makeWildFire)(got2.lannisters.borrowMoney)
+  println(got3.lannisters.wealth.toString, got3.targaryen.wealth.toString) // (WealthImpl(52.019999999999996,10.0),WealthImpl(35.0,30.0))
 }
