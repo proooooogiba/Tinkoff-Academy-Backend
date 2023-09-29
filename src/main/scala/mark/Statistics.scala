2fddb80a2cc68ff672abcfb6b3b3604a53669b7a
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-09-29 13:43:55 +0300

    Сomments have been corrected

--- src/main/scala/mark/Statistics.scala
+++ src/main/scala/mark/Statistics.scala
@@ -4,7 +4,7 @@ import cats.data.NonEmptyList
 
 import java.io.{File, FileWriter}
 import scala.io.Source
-import scala.util.Try
+import scala.util.Using
 
 trait Statistics {
   def sumOpt(student: String): Option[Int]
@@ -22,77 +22,58 @@ trait Statistics {
 
 object Statistics {
   // students - сырые (не обработанные) данные из файла.
+
   def apply(students: List[Student]): Either[Throwable, Statistics] = {
-    try {
-      val originStudents = students
-
-      val marks = originStudents
-        .groupBy(_.name)
-        .view
-        .mapValues(marks => marks.map(_.mark).sum)
-
-      val studentNames = originStudents.view
-        .map(_.name)
-        .distinct
-        .toList
-        .sortBy(name => marks(name))(Ordering[Int].reverse)
-
-      val totalScore = originStudents.map(_.mark).sum
-
-      val sortedScores = originStudents.map(_.mark).sorted
-      val medianScore = {
-        val n   = sortedScores.size
-        val mid = n / 2
-        if (n % 2 == 0) {
-          scala.math.max(sortedScores(mid - 1), sortedScores(mid))
-        } else {
-          sortedScores(mid)
-        }
-      }
-
-      val mostFrequentScore = originStudents
-        .map(_.mark)
-        .groupBy(identity)
-        .maxBy { case (mark, groupOfMark) => (groupOfMark.size, mark) }
-        ._1
-
-      val statistics = new Statistics {
-        val students: NonEmptyList[String] = NonEmptyList.fromListUnsafe(studentNames)
-        val sum: Int                       = totalScore
-        val median: Int                    = medianScore
-        val mostFrequent: Int              = mostFrequentScore
-
-        override def sumOpt(student: String): Option[Int] = {
-          val sumOfMarks = originStudents.map { studentFromList =>
-            if (studentFromList.name == student) studentFromList.mark else 0
-          }.sum
-          if (sumOfMarks > 0) Some(sumOfMarks) else None
-        }
-
-        override def avgOpt(student: String): Option[Double] = {
-          val quantity = originStudents.count(_.name == student)
-          sumOpt(student) match {
-            case Some(sumOfMarks) if quantity != 0 => Some(sumOfMarks.toDouble / quantity)
-            case _                                 => None
+    Either.cond(
+      students.nonEmpty, {
+        val originStudents = students
+        val marks          = students.groupBy(_.name).mapValues(marks => marks.map(_.mark).sum)
+        val studentNames =
+          students.map(_.name).distinct.sortBy(name => marks(name))(Ordering[Int].reverse)
+        val totalScore   = students.map(_.mark).sum
+        val sortedScores = students.map(_.mark).sorted
+        val medianScore = {
+          val n   = sortedScores.size
+          val mid = n / 2
+          if (n % 2 == 0) {
+            scala.math.max(sortedScores(mid - 1), sortedScores(mid))
+          } else {
+            sortedScores(mid)
           }
         }
-      }
-      Right(statistics)
-    } catch {
-      case throwable: Throwable => Left(throwable)
-    }
+        val mostFrequentScore = students
+          .map(_.mark)
+          .groupBy(identity)
+          .maxBy { case (mark, groupOfMark) => (groupOfMark.size, mark) }
+          ._1
+        new Statistics {
+          val students          = NonEmptyList.fromListUnsafe(studentNames)
+          val sum: Int          = totalScore
+          val median: Int       = medianScore
+          val mostFrequent: Int = mostFrequentScore
+
+          override def sumOpt(student: String): Option[Int] = {
+            val sumOfMarks = originStudents.filter(_.name == student).map(_.mark).sum
+            if (sumOfMarks > 0) Some(sumOfMarks) else None
+          }
+
+          override def avgOpt(student: String): Option[Double] = {
+            val quantity = originStudents.count(_.name == student)
+            sumOpt(student).flatMap(sumOfMarks =>
+              if (quantity != 0) Some(sumOfMarks.toDouble / quantity) else None
+            )
+          }
+        }
+      },
+      new Exception("list can't be empty")
+    )
   }
 
   def apply(inputFileName: String): Either[Throwable, Statistics] = {
-    try {
-      val students = readStudentsFromFile(inputFileName) match {
-        case Right(students) if students.isEmpty  => throw new Exception("students can't be empty")
-        case Right(students) if students.nonEmpty => students
-        case Left(error) => throw new RuntimeException(s"Failed to read students from file: $error")
-      }
-      apply(students)
-    } catch {
-      case throwable: Throwable => Left(throwable)
+    readStudentsFromFile(inputFileName) match {
+      case Right(students) if students.isEmpty  => Left(new Exception("students can't be empty"))
+      case Right(students) if students.nonEmpty => apply(students)
+      case Left(error) => Left(new RuntimeException(s"Failed to read students from file: $error"))
     }
   }
 
@@ -100,49 +81,39 @@ object Statistics {
       inputFileName: String,
       outputFileName: String
   ): Either[Throwable, Unit] = {
-    try {
-      val statistics: Statistics = apply(inputFileName) match {
-        case Right(stats) => stats
-        case Left(error)  => throw new RuntimeException(s"Failed to get statistics: $error")
-      }
-      val output = generateOutput(statistics)
-      writeStatsToFile(outputFileName, output) match {
-        case Right(value) =>
-        case Left(error)  => throw new Exception(s"can't write stats to file $error")
-      }
-      Right()
-    } catch {
-      case throwable: Throwable => Left(throwable)
-    }
+    for {
+      statistics <- apply(inputFileName).left.map(error =>
+        new RuntimeException(s"Failed to get statistics: $error")
+      )
+      output = generateOutput(statistics)
+      _ <- writeStatsToFile(outputFileName, output).left.map(error =>
+        new Exception(s"can't write stats to file $error")
+      )
+    } yield ()
   }
 
   private def readStudentsFromFile(inputFileName: String): Either[Throwable, List[Student]] = {
-    try {
-      val source = Source.fromFile(inputFileName)
-
+    Using(Source.fromFile(inputFileName)) { source =>
       val students = (for {
         line <- source.getLines()
         listOfStudent = line.split("\t").toList
-        mark    <- Try(listOfStudent.last.toInt).toOption
+        mark    <- listOfStudent.last.toIntOption
         student <- if (listOfStudent.length == 2) Some(Student(listOfStudent.head, mark)) else None
       } yield student).toList
-
       source.close()
-      Right(students)
-    } catch {
-      case throwable: Throwable => Left(throwable)
-    }
+      students
+    }.toEither
   }
-
   private def writeStatsToFile(outputFileName: String, output: String): Either[Throwable, Unit] = {
     val file = new File(outputFileName)
     if (file.exists()) {
-      throw new Exception("Shouldn't rewrite file")
+      Left(new RuntimeException(s"File $outputFileName already exists"))
+    } else {
+      Using(new FileWriter(file)) { fileWriter =>
+        fileWriter.write(output)
+        fileWriter.close()
+      }.toEither
     }
-    val fileWriter = new FileWriter(file)
-    fileWriter.write(output)
-    fileWriter.close()
-    Right()
   }
 
   private def generateOutput(statistics: Statistics): String = {
@@ -152,8 +123,8 @@ object Statistics {
 
     val studentScores = for {
       student <- statistics.students.toList
-      sumOpt = statistics.sumOpt(student).get
-      avgOpt = statistics.avgOpt(student).get
+      sumOpt = statistics.sumOpt(student).getOrElse(0)
+      avgOpt = statistics.avgOpt(student).getOrElse(0.0)
     } yield f"$student\t$sumOpt\t$avgOpt%.2f"
 
     s"$sum\t$median\t$mostFrequent\n${studentScores.mkString("\n")}"
@@ -161,5 +132,5 @@ object Statistics {
 }
 
 object app extends App {
-  val statistics = Statistics.calculate("input_2.tsv", "output_existed_file.tsv")
+  val statistics = Statistics.calculate("input_1.tsv", "output_test.tsv")
 }
