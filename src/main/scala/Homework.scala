e25fde8f5ee83403bb01a1166c61dabf0b4777ba
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-09-30 09:31:59 +0300

    HW-4 is done

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -1,25 +1,134 @@
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
-import scala.annotation.nowarn
-import scala.io.Source
-import scala.util.Using
-import scala.util.matching.Regex
 
-@nowarn
-object Homework {
+sealed trait Expression
+case class Plus(a: Expression, b: Expression) extends Expression
+case class Diff(a: Expression, b: Expression) extends Expression
+case class Mul(a: Expression, b: Expression)  extends Expression
+case class Div(a: Expression, b: Expression)  extends Expression
+case class Const(v: Int) extends Expression {
+  def plus(other: Const): Const = Const(v + other.v)
 
-  def main(args: Array[String]): Unit = {
-    val constPattern: Regex = "Value\\((-?\\d+)\\)".r
+  def minus(other: Const): Const = Const(v - other.v)
 
-    val matchPattern: String => Either[Exception, Int] = {
-      case constPattern(i) => Right(i.toInt)
-      case _               => Left(new Exception)
-    }
+  def multiply(other: Const): Const = Const(v * other.v)
 
-    Using(Source.fromFile("file.txt")) { s =>
-      s.getLines().toSeq.map(s => s)
-      Files.write(Paths.get("file.txt"), "file contents".getBytes(StandardCharsets.UTF_8))
+  def div(other: Const): Const = Const(v / other.v)
+}
+
+trait Calculate[T] {
+  def calculate(e: T): T
+}
+trait Serializable[T] {
+  def serializable(e: T): String
+}
+
+trait Parseable[T] {
+  def parse(e: String): T
+}
+
+trait Printable[T] {
+  def print(e: T): Unit
+}
+
+object Homework extends App {
+  object Serializable {
+    implicit val expressionSerializble: Serializable[Expression] = _.toString
+  }
+
+  implicit class SerializableSyntax[T](val e: T) {
+    def show(implicit v: Serializable[T]): String = implicitly[Serializable[T]].serializable(e)
+  }
+  object Calculatable {
+    implicit val expressionCalculatable: Calculate[Expression] = new Calculate[Expression] {
+      override def calculate(e: Expression): Const = {
+        e match {
+          case Plus(a, b) => calculate(a).plus(calculate(b))
+          case Diff(a, b) => calculate(a).minus(calculate(b))
+          case Mul(a, b)  => calculate(a).multiply(calculate(b))
+          case Div(a, b)  => calculate(a).div(calculate(b))
+          case c: Const   => c
+        }
+      }
     }
   }
 
+  implicit class CalculatableSyntax[T](val e: T) {
+    def calculate(implicit v: Calculate[T]): T = implicitly[Calculate[T]].calculate(e)
+  }
+
+  object Parseable {
+    def parse[T](e: String): Expression = {
+      var formattedExpression = e
+        .replaceAll("Plus", "+")
+        .replaceAll("Diff", "-")
+        .replaceAll("Div", "/")
+        .replaceAll("Mul", "*")
+        .replaceAll("Const\\((-?\\d+)\\)", "$1")
+
+      def parseIterate(result: String): String = {
+        import Homework.Calculatable.expressionCalculatable
+        val pattern = "(\\+|\\-|\\*|\\/)\\((-?\\d+),(-?\\d+)\\)".r
+        pattern.replaceAllIn(
+          result,
+          m => {
+            val group1 = m.group(1)
+            val group2 = Const(m.group(2).toInt)
+            val group3 = Const(m.group(3).toInt)
+
+            val expr = group1 match {
+              case "+" => Plus(group2, group3)
+              case "-" => Diff(group2, group3)
+              case "*" => Mul(group2, group3)
+              case "/" => Div(group2, group3)
+            }
+            s"${expr.asInstanceOf[Expression].calculate}"
+              .replaceAll("Const\\((-?\\d+)\\)", "$1")
+          }
+        )
+      }
+
+      while (formattedExpression != parseIterate(formattedExpression)) {
+        formattedExpression = parseIterate(formattedExpression)
+      }
+      Const(formattedExpression.toIntOption.getOrElse(0)).asInstanceOf[Expression]
+    }
+
+    implicit val expressionParseableConsole: Parseable[Expression] = (e: String) => {
+      parse(e)
+    }
+
+    implicit val expressionParseableFile: Parseable[Expression] = (e: String) => {
+      parse(e)
+    }
+  }
+
+  object Printable {
+    implicit val expressionPrintableConsole: Printable[Expression] = (e: Expression) => println(e)
+    implicit def expressionPrintableFile(implicit
+        outputFile: String = "file.txt"
+    ): Printable[Expression] =
+      (e: Expression) => {
+        import Homework.Serializable.expressionSerializble
+        Files.write(Paths.get(outputFile), e.show.getBytes(StandardCharsets.UTF_8))
+        ()
+      }
+  }
+
+  def print[A](e: A)(implicit p: Printable[A]): Unit = p.print(e)
+
+  def parse[A](e: String)(implicit p: Printable[A], v: Parseable[A]): Unit = p.print(v.parse(e))
+
+  import Homework.Calculatable.expressionCalculatable
+  import Homework.Parseable.expressionParseableFile
+  import Homework.Printable.expressionPrintableFile
+  import Homework.Serializable.expressionSerializble
+
+  val expr1: Expression = Plus(Const(3), Div(Mul(Const(12), Diff(Const(7), Const(4))), Const(2)))
+  val expr2: Expression = Plus(Plus(Const(3), Const(2)), Diff(Const(5), Const(14)))
+  val expr3: Expression = Plus(Const(5), Plus(Const(10), Const(-1)))
+
+  print(expr1.calculate)(expressionPrintableFile(outputFile = "output.txt"))
+  parse(expr2.show)
+  print(expr3)
 }
