f7c7c130706e7e18a623d8ea3736bf9ab18b750b
Andrey Golikov <a.v.golikov@tinkoff.ru>
2023-10-05 11:44:26 +0300

    Remove obsolete comment

--- src/main/scala/env/package.scala
+++ src/main/scala/env/package.scala
@@ -1,7 +1,5 @@
 import cats.Functor
 
 package object env {
-  // В данном задании нужно реализовать хоть какую-то (но обязательно не бросающую исключения) реализацию для env,
-  // в следующих дз сделаем её правильной.
   def getEnvs[F[_]: Functor: Env](envName: F[String]): F[Option[String]] = ???
 }
